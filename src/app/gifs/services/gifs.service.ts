2079512640dd54f5d1d0b81608e64dde8c0223b1
Cristhian Calderon <cristhian.calderon.mackay@gmail.com>
2023-08-25 14:14:13 +0200

    clase 91

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
   private _tagHistory: string[] = [];
+  private apiKey:string = '9VU1UyMdMeJJ7iN9DNKbnp0zQQMAHV8Q';
+  private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  //api.giphy.com/v1/gifs/search?api_key=9VU1UyMdMeJJ7iN9DNKbnp0zQQMAHV8Q&limit=10&q=valorant
+
+  constructor(private http: HttpClient) { }
 
   get tagHistory() {
     return [...this._tagHistory];
   }
 
-  public searchTag(tag: string):void {
+  private organizeHistory(tag: string){
+    tag = tag.toLowerCase();
+
+    if(this._tagHistory.includes(tag)){
+      this._tagHistory = this._tagHistory.filter( oldTag => oldTag !== tag)
+    }
+
     this._tagHistory.unshift(tag);
+    this._tagHistory = this.tagHistory.splice(0,10);
+
+  }
+
+  public searchTag(tag: string):void {
+    if (tag.length === 0 ) return ;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams().set('api_key',this.apiKey)
+    .set('limit','10')
+    .set('q',tag);
+
+    this.http.get(`${this.serviceUrl}/search`,{params}).subscribe(resp => {
+      console.log(resp)
+    });
+
+
+
   }
 }
